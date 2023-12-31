9e03ea27e9cb0de868030953b9a73f96b7369775
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2023-12-30 21:44:20 -0300

    y

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Instagram'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          'Instagram',
+          style: TextStyle(
+            fontFamily: 'Billabong',
+            fontSize: 40,
+            color: Color.fromRGBO(230, 230, 230, 1),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add_box_outlined),
+            iconSize: 30,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite_border),
+            iconSize: 30,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.chat_bubble_outline),
+            iconSize: 30,
+          )
+        ],
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            width: 80,
+            margin: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: Colors.transparent, // Cor do contorno
+                          width: 1,
+                          style: BorderStyle.solid
+                          // Espessura do contorno
+                          ),
+                      gradient: const LinearGradient(colors: [
+                        Colors.red,
+                        Colors.purple,
+                      ])),
+                  child: const CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 34,
+                      backgroundImage: AssetImage('assets/images/foto_be.jpg'),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 0.0),
+                Text(
+                  'Nome $index',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
